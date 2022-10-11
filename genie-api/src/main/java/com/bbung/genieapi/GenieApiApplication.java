9fb41fec73e4b1d7a3ea29b08a4c7f51bbe66716
bbung95 <tjd123123@naver.com>
2022-10-11 11:10:18 +0900

    feat : project create

---
+++ genie-api/src/main/java/com/bbung/genieapi/GenieApiApplication.java
@@ -0,0 +1,13 @@
+package com.bbung.genieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenieApiApplication.class, args);
+	}
+
+}
---
+++ genie-api/src/test/java/com/bbung/genieapi/GenieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbung.genieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
